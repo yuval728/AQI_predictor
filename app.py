385c830d3344d8be70085fa41a9eedb72f7e7807
vaishnavieieie <vaishnaveekamath@gmail.com>
2025-02-25 13:17:52 +0530

    UI fixes

--- app.py
+++ app.py
@@ -18,21 +18,49 @@ def get_location():
 
 
 def main():
-    st.title("Air Quality Prediction App")
+    st.title("Air Quality Prediction using Satellite Images")
+    st.markdown("This app predicts the Air Quality Index (AQI) using a combination of captured and satellite images.")
     # 2 columns
+    st.subheader("Input Images")
     col1, col2 = st.columns(2)
     
     # sidebar camera
     with st.sidebar:
+        # choose to upload image or capture image
+        st.write("Choose an option to capture an image.")
+        uploaded_image = None
+        # upload image
+        uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+        if uploaded_file:
+            uploaded_image = Image.open(uploaded_file)
+            # st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+
+        st.divider()
         toggle_camera = 'visible'
-        captured_image = st.camera_input("Preview of Street View", key='camera_input', label_visibility=toggle_camera)
+        camera_image = st.camera_input("Preview of Street View", key='camera_input', label_visibility=toggle_camera)
+
         # fetch location
-        st.write("Click the button below to fetch your current location.")
-        lat, lon = get_location()
+        st.divider()
+        col5, col6 = st.columns(2)
+        with col5:
+            st.write("Click the button to fetch your current location.")
+
+        with col6:
+            lat, lon = get_location()
+            # button to clear location
+            if st.button("Clear Location"):
+                lat, lon = None, None
+                
+        
+        if lat and lon:
+            st.success("Location fetched successfully.")
+        else:
+            st.info("Location not captured.")
     
-    if captured_image and lat and lon:
-        image = Image.open(captured_image)   
+    if (uploaded_image!=None or camera_image) and lat and lon:
+        image = uploaded_image if uploaded_image!=None else Image.open(camera_image)  
         captured_width, captured_height = image.size     
+        
         with col1:
             st.image(image, caption="Captured Image", use_container_width=True)
         
@@ -42,14 +70,9 @@ def main():
             # Fetch satellite image
             satellite_image = fetch_satellite(lat, lon)
             satellite_image = Image.open(satellite_image)
-            sat_width, sat_height = satellite_image.size
-            # st.write(f"Satellite Image Size: {sat_width} x {sat_height}")
-            # st.write(f"Captured Image Size: {captured_width} x {captured_height}")
+            # resizing the image to match the captured image
+            satellite_image_resized = satellite_image.resize((captured_width, captured_height))
 
-            # Calculate new width while maintaining aspect ratio
-            new_width = int((captured_height / sat_height) * sat_width)
-            # st.write(f"New Width: {new_width}")
-            satellite_image_resized = satellite_image.resize((new_width, captured_height))
             with col2:
                 st.image(satellite_image_resized, caption="Satellite Image", use_container_width=True)
             
@@ -57,51 +80,67 @@ def main():
             # print(f"Captured Image Type: {type(image)}")
             # print(f"Satellite Image Type: {type(satellite_image)}")
             aqi_result = predict_aqi(image, satellite_image)
-             
-            st.write(f"Location: Latitude {lat}, Longitude {lon}, AQI: {aqi_result}")
 
-            df = pd.DataFrame(
-                data=[[lat, lon, aqi_result]],
-                columns=["lat", "lon", 'aqi'],
-            )
-            
-            # color selection
-            if aqi_result < 50:
-                color = [0, 255, 0]  # Green
-            elif aqi_result < 100:
-                color = [255, 255, 0]  # Yellow
-            elif aqi_result < 150:
-                color = [255, 165, 0]  # Orange
-            else:
-                color = [255, 0, 0]  # Red
-
-            # Create DataFrame
-            df = pd.DataFrame([[lat, lon, aqi_result]], columns=["lat", "lon", "aqi"])
-
-            # Define PyDeck Layer
-            layer = pdk.Layer(
-                "ScatterplotLayer",
-                data=df,
-                get_position=["lon", "lat"],
-                get_color=color,  # Color based on AQI
-                get_radius=200,  # Adjust size
-                pickable=True,  # Enable tooltips
-            )
-
-            # Define the PyDeck Map with a Tooltip
-            view_state = pdk.ViewState(latitude=lat, longitude=lon, zoom=12, pitch=0)
-
-            # Create the PyDeck chart with tooltips
-            st.pydeck_chart(
-                pdk.Deck(
-                    layers=[layer],
-                    initial_view_state=view_state,
-                    tooltip={"text": "AQI: {aqi}"}  # Tooltip displaying AQI value
+            col3, col4 = st.columns(2)
+            with col3:
+                st.subheader("Location Details")
+                st.table({"Latitude": lat, "Longitude": lon})
+                st.subheader("Average result AQI")
+                st.table({"AQI": aqi_result})
+                # dummy table to display the results obtained from different models
+                st.subheader("Model Results")
+                df = pd.DataFrame(
+                    data=[['resnet18', 'some value', aqi_result], ['model2', 'some value', 100], ['model3', 'some value', 100]],
+                    columns=["Model", "Accuracy", "AQI"],
                 )
-            )
+
+                st.table(df)
+
+            with col4:
+                st.subheader("Location on map")
+
+                df = pd.DataFrame(
+                    data=[[lat, lon, aqi_result]],
+                    columns=["lat", "lon", 'aqi'],
+                )
+                
+                # color selection
+                if aqi_result < 50:
+                    color = [0, 255, 0]  # Green
+                elif aqi_result < 100:
+                    color = [255, 255, 0]  # Yellow
+                elif aqi_result < 150:
+                    color = [255, 165, 0]  # Orange
+                else:
+                    color = [255, 0, 0]  # Red
+
+                # Create DataFrame
+                df = pd.DataFrame([[lat, lon, aqi_result]], columns=["lat", "lon", "aqi"])
+
+                # Define PyDeck Layer
+                layer = pdk.Layer(
+                    "ScatterplotLayer",
+                    data=df,
+                    get_position=["lon", "lat"],
+                    get_color=color,  # Color based on AQI
+                    get_radius=200,  # Adjust size
+                    pickable=True,  # Enable tooltips
+                )
+
+                # Define the PyDeck Map with a Tooltip
+                view_state = pdk.ViewState(latitude=lat, longitude=lon, zoom=12, pitch=0)
+
+                # Create the PyDeck chart with tooltips
+                st.pydeck_chart(
+                    pdk.Deck(
+                        layers=[layer],
+                        initial_view_state=view_state,
+                        tooltip={"text": "AQI: {aqi}"}  # Tooltip displaying AQI value
+                    )
+                )
         else:
             st.error("Could not retrieve location. Ensure location access is enabled.")
     else:
-        st.warning("Please capture an image using the camera to proceed.")
+        st.warning("Please capture an image using the camera and give access to location to proceed.")
 if __name__ == "__main__":
     main()
